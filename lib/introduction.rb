83efbd7d39eea07ccf35f782ca2822a894af0619
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-02 10:56:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,7 @@
 def introduction (name)
-  name = "Josh"
   puts "Hi, my name is #{name}."
 end
 
-def introduction (name)
-  name = "Josh"
+def introduction_with_language (name, language)
   puts "Hi, my name is #{name}."
 end
